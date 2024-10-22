50eaf2251062e1b4637e88f6a4ca8f210a20258b
PutraTrianata <putraanata454@gmail.com>
2024-10-22 23:04:07 +0700

    AplikasiPengelolaKeuanganPribadi

---
+++ src/User.java
@@ -0,0 +1,11 @@
+class User {
+    String username;
+    String password;
+    CatatanKeuangan catatanKeuangan;
+
+    User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.catatanKeuangan = new CatatanKeuangan();
+    }
+}
