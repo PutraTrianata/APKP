aaeaf080b875efaaa5b7900fb7a4fa542f152379
PutraTrianata <putraanata454@gmail.com>
2024-10-22 23:11:10 +0700

    AplikasiPengelolaKeuanganPribadi

---
+++ src/Pemasukan.java
@@ -0,0 +1,9 @@
+class Pemasukan {
+    String deskripsi;
+    int jumlah;
+
+    Pemasukan(String deskripsi, int jumlah) {
+        this.deskripsi = deskripsi;
+        this.jumlah = jumlah;
+    }
+}
