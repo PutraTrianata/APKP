a5b289d2d76dcc90da240b6fe3c9a479ba79342a
PutraTrianata <putraanata454@gmail.com>
2024-11-06 00:25:41 +0700

    Update APKP.java

--- src/APKP.java
+++ src/APKP.java
@@ -1,5 +1,6 @@
-public class APKP{
-    public static void main(String[] args){
-        
+public class APKP {
+    public static void main(String[] args) {
+        SistemPengguna sistem = new SistemPengguna(10);
+        // Implementasi tambahan seperti menambahkan pengguna, login, dan lain-lain
     }
 }
