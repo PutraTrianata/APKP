c43c1aa8b088908160e28692300da4669df4809f
PutraTrianata <putraanata454@gmail.com>
2024-11-06 00:23:49 +0700

    Create Transaksi.java

---
+++ src/Transaksi.java
@@ -0,0 +1,17 @@
+class Transaksi {
+    protected String deskripsi;
+    protected int jumlah;
+
+    public Transaksi(String deskripsi, int jumlah) {
+        this.deskripsi = deskripsi;
+        this.jumlah = jumlah;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+}
