19b2a124b95d29d2ffc7560930d13f0207f3a8b3
PutraTrianata <putraanata454@gmail.com>
2024-11-06 00:20:50 +0700

    Update CatatanKeuangan.java

--- src/CatatanKeuangan.java
+++ src/CatatanKeuangan.java
@@ -1,11 +1,23 @@
 class CatatanKeuangan {
-    Pemasukan[] dataPemasukan;
-    Pengeluaran[] dataPengeluaran;
-    Saldo saldo;
+    private Pemasukan[] dataPemasukan;
+    private Pengeluaran[] dataPengeluaran;
+    private Saldo saldo;
 
-    CatatanKeuangan() {
+    public CatatanKeuangan() {
         this.dataPemasukan = new Pemasukan[0];
         this.dataPengeluaran = new Pengeluaran[0];
         this.saldo = new Saldo(0);
+    }
+
+    public void tambahPemasukan(Pemasukan pemasukan) {
+        // Logika untuk menambah pemasukan ke array dataPemasukan
+    }
+
+    public void tambahPengeluaran(Pengeluaran pengeluaran) {
+        // Logika untuk menambah pengeluaran ke array dataPengeluaran
+    }
+
+    public int getSaldo() {
+        return saldo.getJumlah();
     }
 }
