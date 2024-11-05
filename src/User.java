094bb94a9fd867cbd3f518796b62522191404b44
PutraTrianata <putraanata454@gmail.com>
2024-11-06 00:16:39 +0700

    Update User.java
    menambahkan getter dan boolean

--- src/User.java
+++ src/User.java
@@ -1,11 +1,19 @@
 class User {
-    String username;
-    String password;
-    CatatanKeuangan catatanKeuangan;
+    private String username;
+    private String password;
+    private CatatanKeuangan catatanKeuangan;
 
-    User(String username, String password) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
         this.catatanKeuangan = new CatatanKeuangan();
     }
-}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
