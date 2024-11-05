839972e29b5daefe9061544cee27c513a47fa4bf
PutraTrianata <putraanata454@gmail.com>
2024-11-06 00:22:08 +0700

    Update SistemPengguna.java

--- src/SistemPengguna.java
+++ src/SistemPengguna.java
@@ -1,9 +1,27 @@
 class SistemPengguna {
-    User[] users;
-    User loggedInUser;
+    private User[] users;
+    private User loggedInUser;
 
-    SistemPengguna(int kapasitas) {
+    public SistemPengguna(int kapasitas) {
         this.users = new User[kapasitas];
         this.loggedInUser = null;
+    }
+
+    public boolean login(String username, String password) {
+        for (User user : users) {
+            if (user != null && user.getUsername().equals(username) && user.checkPassword(password)) {
+                loggedInUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void logout() {
+        loggedInUser = null;
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
     }
 }
